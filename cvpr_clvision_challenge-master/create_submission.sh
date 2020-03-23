c172882961ffdf9633e2a693fa9a346a2176429a
aobject <sniderjustin@gmail.com>
2020-03-22 22:28:22 -0400

    update submission

--- cvpr_clvision_challenge-master/create_submission.sh
+++ cvpr_clvision_challenge-master/create_submission.sh
@@ -2,7 +2,8 @@
 
 # Run experiments: the submission for each exp will be created in "/submissions"
 # python naive_baseline.py --scenario="ni" --sub_dir="ni"
-python cl_rehearsal.py --scenario="multi-task-nc" --sub_dir="multi-task-nc" --epochs=1 --preload=True
+# python cl_rehearsal.py --scenario="multi-task-nc" --sub_dir="multi-task-nc" --epochs=1 --preload=True
+python cl_ewc.py --scenario="multi-task-nc" --sub_dir="multi-task-nc" --epochs=1 --preload=True
 # python naive_baseline.py --scenario="multi-task-nc" --sub_dir="multi-task-nc" --epochs=1 --preload=True
 # python naive_baseline.py --scenario="nic" --sub_dir="nic"
 
