9cf7741a93ea7d83f32ad2eaaf248d319bc5d91f
aobject <sniderjustin@gmail.com>
2020-03-22 21:21:32 -0400

    ewc in process

--- cvpr_clvision_challenge-master/utils/train_test.py
+++ cvpr_clvision_challenge-master/utils/train_test.py
@@ -251,3 +251,189 @@ def test_multitask(
         classifier = torch.nn.Linear(512, 50)
 
     return stats, preds
+
+# Start Modification
+
+def train_net_ewc(optimizer, model, criterion, mb_size, x, y, t, fisher_dict, optpar_dict, ewc_lambda,
+              train_ep, preproc=None, use_cuda=True, mask=None):
+    """
+    Train a Pytorch model from pre-loaded tensors. 
+    Use EWC to normalize training for CL. 
+
+        Args:
+            optimizer (object): the pytorch optimizer.
+            model (object): the pytorch model to train.
+            criterion (func): loss function.
+            mb_size (int): mini-batch size. we use 32. 
+            x (tensor): train data.
+            y (tensor): train labels.
+            t (int): task label.
+            train_ep (int): number of training epochs.
+            preproc (func): test iterations.
+            use_cuda (bool): if we want to use gpu or cpu.
+            mask (bool): if we want to maks out some classes from the results.
+        Returns:
+            ave_loss (float): average loss across the train set.
+            acc (float): average accuracy over training.
+            stats (dict): dictionary of several stats collected.
+    """
+
+    cur_ep = 0
+    cur_train_t = t
+    stats = {"ram": [], "disk": []}
+
+    if preproc:
+        x = preproc(x)
+
+    (train_x, train_y), it_x_ep = pad_data(
+        [x, y], mb_size
+    )
+
+    shuffle_in_unison(
+        [train_x, train_y], 0, in_place=True
+    )
+
+    model = maybe_cuda(model, use_cuda=use_cuda)
+    acc = None
+    ave_loss = 0
+
+    train_x = torch.from_numpy(train_x).type(torch.FloatTensor)
+    train_y = torch.from_numpy(train_y).type(torch.LongTensor)
+
+    for ep in range(train_ep):
+
+        stats['disk'].append(check_ext_mem("cl_ext_mem"))
+        stats['ram'].append(check_ram_usage())
+
+        model.active_perc_list = []
+        model.train()
+
+        print("training ep: ", ep)
+        correct_cnt, ave_loss = 0, 0
+        for it in range(it_x_ep):
+
+            start = it * mb_size
+            end = (it + 1) * mb_size
+
+            optimizer.zero_grad()
+
+            x_mb = maybe_cuda(train_x[start:end], use_cuda=use_cuda)
+            y_mb = maybe_cuda(train_y[start:end], use_cuda=use_cuda)
+            logits = model(x_mb)
+
+            _, pred_label = torch.max(logits, 1)
+            correct_cnt += (pred_label == y_mb).sum()
+
+            loss = criterion(logits, y_mb)
+            ave_loss += loss.item()
+
+            # Start Modification
+
+            # Add EWC Penalty
+            if t != 0:
+                # use EWC
+                for name, param in model.named_parameters(): # for each weight 
+                    fisher = fisher_dict[name]  # get the fisher value for the given task and weight
+                    optpar = optpar_dict[name]  # get the parameter optimized value for the given task and weight
+                    loss += (fisher * (optpar - param).pow(2)).sum() * ewc_lambda  # loss is accumulator # add penalty for current task and weight
+
+            # End Modification
+
+            loss.backward()
+            optimizer.step()
+
+            acc = correct_cnt.item() / \
+                  ((it + 1) * y_mb.size(0))
+            ave_loss /= ((it + 1) * y_mb.size(0))
+
+            if it % 100 == 0:
+                print(
+                    '==>>> it: {}, avg. loss: {:.6f}, '
+                    'running train acc: {:.3f}'
+                        .format(it, ave_loss, acc)
+                )
+
+        cur_ep += 1
+
+    return ave_loss, acc, stats
+
+
+# Function to comput the fisher information for each weight at the end of each task
+def on_task_update(t, x, y, fisher_dict, optpar_dict, model, optimizer, criterion, mb_size, use_cuda=True, mask=None, preproc=None):
+    """
+    INPUT:
+        task_id: integer representing the task number
+        x_mem:  current x_train values
+        t_mem:  current true y_train values (aka target values)
+
+    OUTPUT: 
+        The new values are added to the fisher and optpar dictionaries. 
+        fisher_dict[task_id]  
+        optpar_dict[task_id] 
+
+    """
+    cur_ep = 0
+    cur_train_t = t
+
+    if preproc:
+        x = preproc(x)
+
+    (train_x, train_y), it_x_ep = pad_data(
+        [x, y], mb_size
+    )
+
+    # shuffle_in_unison(
+    #     [train_x, train_y], 0, in_place=True
+    # )
+
+    model = maybe_cuda(model, use_cuda=use_cuda)
+    acc = None
+    ave_loss = 0
+
+    train_x = torch.from_numpy(train_x).type(torch.FloatTensor)
+    train_y = torch.from_numpy(train_y).type(torch.LongTensor)
+
+    model.active_perc_list = []
+    model.train()  # model in train mode 
+  
+    # loop through batches
+    # prepare minibatch
+    # get loss
+    print("Updating Fisher values and old parameters")
+    correct_cnt, ave_loss = 0, 0
+    for it in range(it_x_ep):
+
+        start = it * mb_size
+        end = (it + 1) * mb_size
+
+        optimizer.zero_grad()
+
+        x_mb = maybe_cuda(train_x[start:end], use_cuda=use_cuda)
+        y_mb = maybe_cuda(train_y[start:end], use_cuda=use_cuda)
+        logits = model(x_mb)
+
+        _, pred_label = torch.max(logits, 1)
+        correct_cnt += (pred_label == y_mb).sum()
+
+        loss = criterion(logits, y_mb)
+        ave_loss += loss.item()
+        loss.backward()
+
+        # Update optpar_dict and fisher_dict for EWC
+        for name, param in model.named_parameters():  # for every parameter save two values 
+            optpar = param.data.clone()  # save optimized gradient value for current task i and current gradient location j
+            fisher = param.grad.data.clone().pow(2)  # save fisher value for current task i and current gradient location j 
+            if t == 0:  # first task. Just save weights and fisher values for next round
+                optpar_dict[name] = optpar
+                fisher_dict[name] = fisher
+            else:
+                optpar_dict[name] = optpar  # save weights for next round
+                fisher_dict[name] = (((fisher_dict[name]/(t+1))*t) + (fisher / (t+1)))  # average together old and new fisher values. save for use on next training round. 
+
+
+# End Modification        
+
+
+
+
+
